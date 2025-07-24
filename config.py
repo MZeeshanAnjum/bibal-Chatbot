441a1012a188a80b408b718b26de7370ba24ca38
MZeeshanAnjum <129945652+MZeeshanAnjum@users.noreply.github.com>
2025-07-24 18:02:45 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+####################################
+# Load .env file
+####################################
+
+try:
+    from dotenv import load_dotenv, find_dotenv
+
+    load_dotenv(find_dotenv("./.env"))
+except ImportError:
+    print("dotenv not installed, skipping...")
+
+API_KEY = os.getenv("PIPELINES_API_KEY", "0p3n-w3bu!")
+PIPELINES_DIR = os.getenv("PIPELINES_DIR", "./pipelines")
+
+LOG_LEVELS = {"INFO": 20, "DEBUG": 10}
